ce8e48d4b926405c4966419563c94398f7abc1a8
Raphaelngowa <raphael.ngowa@gmail.com>
2023-07-13 16:45:09 -0700

    This was pretty recursive

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Computer types printing program
+ * Return 0 (Wow!)
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int l;
+	long long int d;
+	float f;
+printf("Char size: %lu byte(s)\n", (unsigned long)sizeof(C));
+printf("Char size: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("Char size: %lu byte(s)\n", (unsigned long)sizeof(l));
+printf("Char size: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Char size: %lu byte(s)\n", (unsigned long)sizeof(f));
+return (0);
+}

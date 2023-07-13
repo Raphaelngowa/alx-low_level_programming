f709691d699911826538dda40163a15d2667fe84
Raphaelngowa <raphael.ngowa@gmail.com>
2023-07-13 16:13:52 -0700

    Finallly

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - Printf function line printing program
+ *Return: 0 (Great)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}

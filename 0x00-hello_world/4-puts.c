6cc1efe7106ccf3e5b2abafa8e846a6b5c060eb9
Raphaelngowa <raphael.ngowa@gmail.com>
2023-07-13 15:32:20 -0700

    Just work yoh

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *  main - Puts function line printing prigram
+ *  Return: 0 (Excellent)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

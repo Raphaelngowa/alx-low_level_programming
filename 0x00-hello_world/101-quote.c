863ac7ce0b8fb8d740711580f14289951d3a348d
Raphaelngowa <raphael.ngowa@gmail.com>
2023-07-13 17:28:57 -0700

    wueh

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <std.io>
+#include <unistd.h>
+/**
+ * main - Standard error line printing program
+ * Return 1 (Great)
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}

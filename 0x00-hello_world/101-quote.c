34f18270900d9a64ddbbf5be331abc400e098da6
Raphaelngowa <raphael.ngowa@gmail.com>
2023-07-13 17:53:24 -0700

    Mymy

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
-#include <std.io>
+#include <stdio.h>
 #include <unistd.h>
 
 /**
- * main - Standard error line printing program
+ * main - Entry point
  *
- * Return: 1 (Great)
+ * Return: Always 1 (Excellent)
  */
 int main(void)
 {
-        write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 24);
-        return (1);
-}
+	write(2,
+			"and that piece of art is useful\" - Dora Korpa, 2015-10-19\n", 59);
+			return (1);

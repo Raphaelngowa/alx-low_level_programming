9d9ea6d088376f1f01fbeb77e5e40afc18aa50e8
Raphaelngowa <raphael.ngowa@gmail.com>
2023-07-17 05:17:00 -0700

    Update

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,3 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * main - Prints all lower cases a - z except q&e.
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+if (letter != 'q' && letter != 'e')
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}

966afd4c712eb88604cd1591bb5852acaf90cb41
Raphaelngowa <raphael.ngowa@gmail.com>
2023-07-18 13:43:03 -0700

    Update

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints alphabet in lower case followed by a new line
+ *
+ * Return:void
+ */
+
+void print_alphabet(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}

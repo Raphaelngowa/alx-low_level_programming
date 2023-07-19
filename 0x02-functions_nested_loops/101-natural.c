9c1421ee36448576428bf72873bc22dcb8e5ea6f
Raphaelngowa <raphael.ngowa@gmail.com>
2023-07-19 09:08:08 -0700

    Update

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -  Computes & prints sum of mulltiples of 3 or 5 below 1024
+ * Return: Always (Runs)
+ */
+
+int main(void)
+{
+int i, z = 0;
+while (i < 1024)
+{
+if ((i % 3 == 0) || (i % 5 == 0))
+{
+	z += i;
+}
+i++
+}
+printf("%d\n", z);
+return (0);
+}

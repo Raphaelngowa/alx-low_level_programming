bcb22451ff863c8742abfa5ab472f0a64461de5b
Raphaelngowa <raphael.ngowa@gmail.com>
2023-07-20 16:18:11 -0700

    Isuper

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: multiplies with @b
+ * @b: multiplies with @a
+ * Return: Ever 0
+ */
+
+int mul(int a, int b)
+{
+int result = a * b;
+return (result);
+}

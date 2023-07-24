bf33337701a1c49697aecb4e09566296a551a23f
Raphaelngowa <raphael.ngowa@gmail.com>
2023-07-24 12:09:08 -0700

    Update

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* swap_int - swaps the value of int a & b
+* @a: first int
+* @b: second int
+* Return: Always 0 (Success)
+*/
+
+void swap_int(int *a, int *b)
+{
+int s;
+s = *a;
+*a = *b;
+*b = s;
+}

151784af378c494c37f685e91efb2a57efca08e7
Raphaelngowa <raphael.ngowa@gmail.com>
2023-07-24 09:16:56 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-reset_to_98.c -o 0-98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - Takes a pointer to an it & updates value point to 98
+ * @n:Update value to 98
+ * Return: Ever 0(Succes)
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
+

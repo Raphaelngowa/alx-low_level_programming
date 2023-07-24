4dfeeb6246ef9551f86635339b7d05aae8598bfe
Raphaelngowa <raphael.ngowa@gmail.com>
2023-07-24 16:29:51 -0700

    Agh

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,17 +2,16 @@
 
 /**
  * _puts - prints a string followed by a new.
- * @str: the string
- *Return: Void
+ * @str: the string*/
+ int _putchar(char c);
+ /*Return: Void
 */
 
-void _puts(char *str)
-{
+void _puts(const char *str) {
 int i = 0;
-while (str[i])
+while (str[i] != '\0') 
 {
 _putchar(str[i]);
 i++;
 }
-_putchar('\n');
 }

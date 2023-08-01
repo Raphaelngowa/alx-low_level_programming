21a333367a95b0da567d4bed4d03acc71d4d5713
Raphaelngowa <raphael.ngowa@gmail.com>
2023-08-01 15:39:54 -0700

    Update

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * set_string - sets the value of a pointer to a character
+ * @s: pointer to change
+ * @to: string to change pointer to car
+ *
+ * Return: nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
